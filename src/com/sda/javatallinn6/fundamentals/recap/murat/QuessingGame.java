2986522046d57b165e9ac3f412daba6ac240d5e9
Anneli Rasu <annelirasu@gmail.com>
2019-04-27 19:32:52 +0300

    Improvement of the Quessing Game

--- src/com/sda/javatallinn6/fundamentals/recap/murat/QuessingGame.java
+++ src/com/sda/javatallinn6/fundamentals/recap/murat/QuessingGame.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class QuessingGame {
     public static void main(String[] args) {
         Scanner scan= new Scanner(System.in);
-        System.out.println("Welcome stranger. Pelase Enter your full name:");
+        System.out.println("Welcome stranger. Please enter your full name:");
         String answer=scan.nextLine();
         if(answer.equalsIgnoreCase("james bond")){
-            System.out.println("Welcome 007");
+            System.out.println("Welcome 007!");
         }else {
             System.out.println("Get out, you are not an agent!");
         }
